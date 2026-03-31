a0f2dde0489a0934c004eef2e5b01f367527f77e
drtusharkanani <drtusharkanani@gmail.com>
2026-03-31 21:17:26 +0530

    register.js

--- api/register.js
+++ api/register.js
@@ -76,7 +76,7 @@ export default async function handler(req, res) {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            from:    'GoodReview <noreply@goodreview.in>',
+            from: 'GoodReview <onboarding@resend.dev>',
             to:      body.email,
             subject: `Welcome to GoodReview, ${body.name}!`,
             html: `
